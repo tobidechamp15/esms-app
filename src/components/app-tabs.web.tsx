000e7f542eb2797550d542cf17e220a154b661e2
Oluwatobiloba Oluwadare <tobidechamp15@gmail.com>
2026-06-11 00:06:41 +0100

    dev changes

--- src/components/app-tabs.web.tsx
+++ src/components/app-tabs.web.tsx
@@ -24,8 +24,8 @@ export default function AppTabs() {
           <TabTrigger name="home" href="/" asChild>
             <TabButton>Home</TabButton>
           </TabTrigger>
-          <TabTrigger name="explore" href="/explore" asChild>
-            <TabButton>Explore</TabButton>
+          <TabTrigger name="visits" href="/visits" asChild>
+            <TabButton>Visits</TabButton>
           </TabTrigger>
         </CustomTabList>
       </TabList>
